af4d0b6911bd09c17386a8f3625e51712873ca0b
Yuko <yyamano@my.centennialcollege.ca>
2021-09-15 22:12:35 -0400

    solution added

--- photo.js
+++ photo.js
@@ -1,46 +1,62 @@
-class Photo {
-    constructor(width = 8, height = 10) {
-      this.width = width;
-      this.height = height;
+export class Photo {
+    constructor(width = 8, height = 10){
+        this.width = width;
+        this.height = height;
     }
-  
+    
     price() {
-      let finalPrice;
-  
-      if (this.width === 8 && this.height === 10) {
-          finalPrice = 4;
-      } else {
-          if (this.width === 10 && this.height === 12) {
-              finalPrice = 6;
-          } else {
-              finalPrice = 10;
-          }
-      }
-  
-      return finalPrice;
+        let finalPrice;
+
+        if ((this.width == 8) && (this.height == 10)) {
+            finalPrice = 4;
+        } else {
+            if ((this.width == 10) && (this.height == 12)) {
+                finalPrice = 6;
+            } else {
+                finalPrice = 10;
+            }
+        }
+
+        return finalPrice;
     }
-  
-    toString() {
+
+    toString(){
         return `This is a ${this.width} by ${this.height} photo and it costs ${this.price()}`;
     }
-  }
-  
-  let photo1 = new Photo(8, 10);
-  console.log(photo1.toString());
-  
-  let photo2 = new Photo(20, 30);
-  console.log(photo2.toString());
+}
 
-  class MattedPhoto{
-    super (width,height,color){
+export class MattedPhoto extends Photo {
+    constructor(width, height, color){
+        super(width, height);
         this.color = color;
     }
-  }
 
-  class FramedPhoto{
-      super (width,height,material,style){
-          this.material = material;
-          this.style = style;
-      }
+    price() {
+        const addedPrice = 10;
+        return super.price() + addedPrice;
+    }
+    
+    toString(){
+        return `This is a ${this.width} by ${this.height} matted ${this.color} photo and it costs ${this.price()}`;
+    }
+}
 
-  }
+
+export class FramedPhoto extends Photo {
+    
+    constructor(width, height, material, style){
+        super(width, height);
+        this.material = material;
+        this.style = style;
+    }
+
+    price() {
+        const addedPrice = 25;
+        return super.price() + addedPrice;
+    }
+    
+    toString(){
+        return `This is a ${this.width} by ${this.height} ${this.material} framed photo. The style is ${this.style} and it costs ${this.price()}`;
+    }
+}
+
